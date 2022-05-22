34c15c1de14c48062080de73c97d2926df8358e2
Ahtesham Ibne Mostafa <ahteshamibnemostafa@gmail.com>
2022-05-23 00:18:56 +0600

    up

---
+++ Circle.py
@@ -0,0 +1,5 @@
+import math
+radius = float ( input ( 'please enter the radius value :'))
+
+print ( 'The area of the circle is :', math.pi * radius **2 )
+print ( 'The ambit of the circle is :', math.pi * radius * 2 )
---
+++ task6.py
@@ -0,0 +1,11 @@
+x= int (input('whats your mark in exam :'))
+if (x >= 33):
+    if (x >= 70):
+        if (x >= 80):
+            print ('A+')
+        else:
+                print ('A')
+    else:
+        print ('pass')
+else :
+    print ('fail')
